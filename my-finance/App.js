4d2b6e0d08ef6159bf648ab56f2c5509a6d8d392
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-03 00:28:18 -0300

    init navigation

--- my-finance/App.js
+++ my-finance/App.js
@@ -1,10 +1,15 @@
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import firebase from './src/services/firebaseConnection';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View>
-      <Text style={{marginTop: 20}}>eh</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#131313" barStyle="litgh-content"/>
+      <Routes/>
+    </NavigationContainer>
   );
 }
