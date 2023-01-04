1448a9001ec72a34d21322c5c0e3b666cc5c89f3
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-03 23:41:20 -0300

    finish signIn page and init signUp page

--- my-finance/src/pages/SignIn/index.js
+++ my-finance/src/pages/SignIn/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Platform } from 'react-native';
 import { Background, Container, AreaInput, Logo, Input, SubmitButton, SubmitText, Link, LinkText } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export default function SignIn() {
- return (
+  const navigation = useNavigation();
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const sendForm = () => {
+    alert(`${email}  ${password}`);
+  };
+ 
+  return (
    <Background>
-    <Container>
+    <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled >
       <Logo source={require('../../assets/logo.png')}/>
 
       <AreaInput>
@@ -13,6 +23,8 @@ export default function SignIn() {
           placeholder="Email..."
           autoCorrect={false}
           autoCapitalize="none"
+          value={email}
+          onChangeText={ email => setEmail(email) }
         />
       </AreaInput>
       <AreaInput>
@@ -20,13 +32,15 @@ export default function SignIn() {
           placeholder="Senha..."
           autoCorrect={false}
           autoCapitalize="none"
+          value={password}
+          onChangeText={ password => setPassword(password) }
         />
       </AreaInput>
-      <SubmitButton>
+      <SubmitButton onPress={sendForm}>
         <SubmitText>Entrar</SubmitText>
       </SubmitButton>
 
-      <Link>
+      <Link onPress={ () => navigation.navigate('SignUp') } >
         <LinkText>Não possui uma conta? Cadastre-se aqui!</LinkText>
       </Link>
 
--- my-finance/src/routes/auth.routes.js
+++ my-finance/src/routes/auth.routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 const AuthStack = createStackNavigator();
 
@@ -9,7 +10,11 @@ export default function AuthRoutes() {
   return(
     <AuthStack.Navigator>
       <AuthStack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }}/>
+
+      <AuthStack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }}/>
     </AuthStack.Navigator>
+
+    
   );
 }
 
