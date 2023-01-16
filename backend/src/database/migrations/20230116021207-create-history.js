e8dd2a2887c6341e35491589e30f0a8a286ca188
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-15 23:17:51 -0300

    created history migration

---
+++ backend/src/database/migrations/20230116021207-create-history.js
@@ -0,0 +1,46 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('history', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      comment: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('history');
+  },
+};
