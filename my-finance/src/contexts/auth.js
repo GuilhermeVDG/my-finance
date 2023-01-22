7a7cd16c7eb3122cd599938d7347d8a5bd84efcd
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-22 17:33:16 -0300

    changed context to local api

--- my-finance/src/contexts/auth.js
+++ my-finance/src/contexts/auth.js
@@ -1,17 +1,16 @@
 import React, { createContext, useState, useEffect } from "react";
-import firebase from '../services/firebaseConnection';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { api } from "../services/api";
 
 export const AuthContext = createContext();
 
 export default function AuthProvider ({ children }) {
-  const [user, setUser] = useState(
-    'id',
-    'name',
-    'email',
-    'token'
-  );
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    token: ''
+  });
   const [loading, setLoading] = useState(true);
   const [authLoading, setAuthLoading] = useState(false);
 
@@ -30,29 +29,20 @@ export default function AuthProvider ({ children }) {
 
   const signUp = async (name, email, password) => {
     setAuthLoading(true);
-    await firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(async value => {
-        const uid = value.user.uid;
-        await firebase.database().ref('users').child(uid).set({
-          amount: 0,
-          name: name
-        })
-          .then(() => {
-            const data = {
-              uid: uid,
-              name: name,
-              email: value.user.email
-            }
-            setUser(data)
-            storeUser(data);
-            setAuthLoading(false);
-          })
-          .catch(err => {
-            console.log(err);
-            alert('Falha ao cadastrar.');
-            setAuthLoading(false);
-          });
-      })
+    
+    try {
+      const response = await api.post('/store', {
+        name,
+        email,
+        password
+      });
+
+      await signIn(email, password);
+    } catch (error) {
+      console.log(error);
+      setAuthLoading(false);
+    }
+
   }
 
   const storeUser = async data => {
@@ -74,12 +64,12 @@ export default function AuthProvider ({ children }) {
 
       api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
-      setUser(
+      setUser({
         id,
         name,
         email,
         token
-      )
+      })
 
       setAuthLoading(false);
     } catch (error) {
@@ -89,11 +79,11 @@ export default function AuthProvider ({ children }) {
   }
 
   const signOut = async () => {
-    await firebase.auth().signOut();
-    const token = await AsyncStorage.getItem('auth');
-    console.log(token);
-    await AsyncStorage.clear()
-      .then(() => setUser(null));
+    try {
+      await AsyncStorage.clear();
+    } catch (error) {
+      console.log(error);
+    }
   }
   
   return(
