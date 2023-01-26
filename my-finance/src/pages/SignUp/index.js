95bc7fbb3e2ee037e5074e00348c6b8cd026e9d2
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-26 20:30:21 -0300

    fixed signUp keyboard dismiss

--- my-finance/src/pages/SignUp/index.js
+++ my-finance/src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Platform, ActivityIndicator } from 'react-native';
+import { Platform, ActivityIndicator, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Background, Container, AreaInput, Logo, Input, SubmitButton, SubmitText, Link, LinkText } from '../SignIn/styles';
 import { AuthContext } from '../../contexts/auth';
@@ -18,49 +18,51 @@ export default function SignUp() {
   };
  
   return (
-  <Background>
-  <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled >
-    <Logo source={require('../../assets/logo.png')}/>
+    <Background>
+      <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+        <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled >
+          <Logo source={require('../../assets/logo.png')}/>
 
-    <AreaInput>
-      <Input
-        placeholder="Nome..."
-        autoCorrect={false}
-        autoCapitalize="none"
-        value={name}
-        onChangeText={ name => setName(name) }
-      />
-    </AreaInput>
-    
-    <AreaInput>
-      <Input
-        placeholder="Email..."
-        autoCorrect={false}
-        autoCapitalize="none"
-        value={email}
-        onChangeText={ email => setEmail(email) }
-      />
-    </AreaInput>
-    
-    <AreaInput>
-      <Input
-        placeholder="Senha..."
-        autoCorrect={false}
-        autoCapitalize="none"
-        value={password}
-        onChangeText={ password => setPassword(password) }
-        secureTextEntry={true}
-      />
-    </AreaInput>
-    <SubmitButton onPress={sendForm}>
-      { authLoading ? <ActivityIndicator size={20} color="#000000" /> : <SubmitText>Cadastrar</SubmitText> }
-    </SubmitButton>
+          <AreaInput>
+            <Input
+              placeholder="Nome..."
+              autoCorrect={false}
+              autoCapitalize="none"
+              value={name}
+              onChangeText={ name => setName(name) }
+            />
+          </AreaInput>
+          
+          <AreaInput>
+            <Input
+              placeholder="Email..."
+              autoCorrect={false}
+              autoCapitalize="none"
+              value={email}
+              onChangeText={ email => setEmail(email) }
+            />
+          </AreaInput>
+          
+          <AreaInput>
+            <Input
+              placeholder="Senha..."
+              autoCorrect={false}
+              autoCapitalize="none"
+              value={password}
+              onChangeText={ password => setPassword(password) }
+              secureTextEntry={true}
+            />
+          </AreaInput>
+          <SubmitButton onPress={sendForm}>
+            { authLoading ? <ActivityIndicator size={20} color="#000000" /> : <SubmitText>Cadastrar</SubmitText> }
+          </SubmitButton>
 
-    <Link onPress={ () => navigation.navigate('SignIn') } >
-      <LinkText>Já possui uma conta? Entre aqui!</LinkText>
-    </Link>
+          <Link onPress={ () => navigation.navigate('SignIn') } >
+            <LinkText>Já possui uma conta? Entre aqui!</LinkText>
+          </Link>
 
-  </Container>
- </Background>
+        </Container>
+      </TouchableWithoutFeedback>
+    </Background>
   );
 }
