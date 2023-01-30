25bbf9a11e338ddbff9d44e809e4856508bf5ea0
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-30 19:22:00 -0300

    fixed amount on home page

--- my-finance/src/pages/Home/index.js
+++ my-finance/src/pages/Home/index.js
@@ -10,12 +10,7 @@ import { Background, Container, Name, Amount, Title, List } from './styles';
 
 export default function Home({ route }) {
   const [history, setHistory] = useState([]);
-  const [user, setUser] = useState({
-    id: '',
-    name: '',
-    email: '',
-    amount: 0
-  });  
+  const { user } = useContext(AuthContext);
 
 
   useEffect(() => {
@@ -23,21 +18,15 @@ export default function Home({ route }) {
       const listHist = await api.get('/history/list');
       setHistory(listHist.data.body);
     }
-
-    const loadUser = async () => {
-      const findUser = await api.get('/user/detail');
-      setUser(findUser.data.body);
-    }
     loadHistory();
-    loadUser();
-  }, [route.params]);
+  }, []);
 
   return (
     <Background>
       <Header/>
       <Container>
-        <Name>{user.name}</Name>
-        <Amount>R$ {user.amount ? user.amount.toFixed(2) : ''}</Amount>
+        <Name>{user.user.name}</Name>
+        <Amount>R$ {user.user.amount ? user.user.amount.toFixed(2) : ''}</Amount>
       </Container>
 
       <Title>Ultimas operações</Title>
