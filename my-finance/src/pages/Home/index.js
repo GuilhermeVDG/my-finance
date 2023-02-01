71829bf4a557286c98f8e28f10e93c31540cb076
Guilherme Vanderley <guivdouradog@gmail.com>
2023-01-31 23:14:54 -0300

    fixed load user

--- my-finance/src/pages/Home/index.js
+++ my-finance/src/pages/Home/index.js
@@ -3,30 +3,44 @@ import Header from '../../components/Header';
 import ListHistory from '../../components/ListHistory';
 import { api } from '../../services/api';
 import { format } from 'date-fns';
-import { AuthContext } from '../../contexts/auth';
+
 
 import { Background, Container, Name, Amount, Title, List } from './styles';
 
 
 export default function Home({ route }) {
   const [history, setHistory] = useState([]);
-  const { user } = useContext(AuthContext);
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    email: '',
+    amount: 0
+  }); 
 
 
   useEffect(() => {
     const loadHistory = async () => {
-      const listHist = await api.get('/history/list');
-      setHistory(listHist.data.body);
+      try {
+        const listHist = await api.get('/history/list');
+        setHistory(listHist.data.body);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    const loadUser = async () => {
+      const findUser = await api.get('/user/detail');
+      setUser(findUser.data.body);
     }
     loadHistory();
-  }, []);
+    loadUser();
+  }, [route.params]);
 
   return (
     <Background>
       <Header/>
       <Container>
-        <Name>{user.user.name}</Name>
-        <Amount>R$ {user.user.amount ? user.user.amount.toFixed(2) : ''}</Amount>
+        <Name>{user.name}</Name>
+        <Amount>R$ {user.amount ? user.amount.toFixed(2) : ''}</Amount>
       </Container>
 
       <Title>Ultimas operações</Title>
